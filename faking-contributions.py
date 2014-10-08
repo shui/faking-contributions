7594438141c17e568191bb8d0a9768896ff5edf4
Syrexide <justice.suh@gmail.com>
2014-10-08 14:14:46 -0400

    rewrote in python

---
+++ faking-contributions.py
@@ -0,0 +1,40 @@
+import sys, os
+from subprocess import call
+from datetime import date, timedelta
+from random import randint
+
+username = sys.argv[1]
+password = sys.argv[2]
+repository = sys.argv[3]
+hasTemplate = False
+if len(sys.argv) == 5:
+	template = sys.argv[4]
+	hasTemplate = True
+
+call('git init')
+call('git remote add origin https://' + username + ':' + password + '@github.com/' + username + '/' + repository + '.git')
+
+time = '12:00:00'
+offset = '-0500'
+minimum = 0
+maximum = 50
+filename = 'README.md'
+message = 'test'
+
+half = ' ' + time + ' ' + offset
+current = date.today() - timedelta(days=365)
+if hasTemplate:
+	print 'Templating not yet supported'
+else:
+	for i in range(0, 365):
+		for k in range(0, randint(minimum, maximum)):
+			complete = str(current) + half + ':' + str(k)
+			readme = open(filename, 'w+')
+			readme.write(complete)
+			# message = complete
+			call('git add ' + filename)
+			call('git commit --date="' + complete + '" -m "' + message + '"', stdout=open(os.devnull, 'w'))
+			print 'Committing: ' + complete
+		call('git push origin master')
+		current = current + timedelta(days=1)
+readme.close()
